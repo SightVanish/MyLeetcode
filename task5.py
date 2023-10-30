88c7d23ae46dfb808fa725b11cd8485b6ebfd27a
liwuchen <liwuchen@florens.com>
2023-10-30 13:29:28 +0800

    add more

--- task5.py
+++ task5.py
@@ -7,40 +7,15 @@ Explanation: "aba" is also a valid answer.
 """
 
 class Solution:
-    def getPalindrome(self, s, i, j):
-        # expand palindromic string from i<-center->j
-        while i >=0 and j < len(s) and s[i] == s[j]:
-            i -= 1
-            j += 1
-        return i+1, j-1 # you can not return i, j here
     def longestPalindrome(self, s: str) -> str:
-        i, j = 0, 0
-        for center in range(len(s)):
-            # there are two situation for Palindrome
-            l1, r1 = self.getPalindrome(s, center, center) # odd palindromic string
-            l2, r2 = self.getPalindrome(s, center, center+1) # even palindromic string
-            # keep track of the longest palindromic string
-            if r1-l1 > j-i:
-                i, j = l1, r1
-            if r2-l2 > j-i:
-                i, j = l2, r2
-        return s[i:j+1] # note: j+1 here
-
-
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
+        l, r = 0, 0
         def expand(i, j):
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                i -= 1
-                j += 1
-            return i + 1, j - 1
-        l, r = 0, 0
-        for pivot in range(len(s) - 1):
-            l1, r1 = expand(pivot, pivot)
-            l2, r2 = expand(pivot, pivot + 1)
-            if r1 - l1 > r - l:
-                l, r = l1, r1
-            if r2 - l2 > r - l:
-                l, r = l2, r2
-        return s[l: r+1]
-
+                i -= 1; j += 1
+            return i+1, j-1
+        for p in range(len(s)-1):
+            i, j = expand(p, p)
+            l, r = (i, j) if j - i > r - l else (l, r)
+            i, j = expand(p, p+1)
+            l, r = (i, j) if j - i > r - l else (l, r)
+        return s[l:r+1]

13254642b12eda819b5e1849fa9f3a1b40dd2908
liwuchen <liwuchen@florens.com>
2023-11-13 13:49:44 +0800

    add more

---
+++ task2795.py
@@ -0,0 +1,20 @@
+"""
+Given a 0-indexed string s, permute s to get a new string t such that:
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+"""
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        s = list(s)
+        vowel = sorted([c for c in s if c.lower() in 'aeiou'])
+        j = 0
+        for i in range(len(s)):
+            if s[i].lower() in 'aeiou':
+                s[i] = vowel[j]
+                j += 1
+        return ''.join(i for i in s)
+
+s = Solution()
+print(s.sortVowels(s = "lEetcOde"))

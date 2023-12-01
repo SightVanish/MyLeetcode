b15fe28f9122333cd759070f7529c2ece5e3b2e0
liwuchen <liwuchen@florens.com>
2023-12-01 10:29:13 +0800

    update

---
+++ task1662.py
@@ -0,0 +1,11 @@
+"""
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+"""
+from typing import List
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+
+s = Solution()
+print(s.arrayStringsAreEqual(word1 = ["ab", "c"], word2 = ["a", "bc"]))

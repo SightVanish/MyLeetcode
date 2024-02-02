03d97638f572383fc501cb1acce523f2c1605542
liwuchen <liwuchen@florens.com>
2024-02-02 13:20:24 +0800

    add more

--- task1291.py
+++ task1291.py
@@ -6,10 +6,22 @@ from typing import List
 
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        
+        res = []
+        for i in range(len(str(low)), len(str(high))+1):
+            for j in range(1, 11-i):
+                c = int(''.join([str(j+m) for m in range(i)]))
+                if low <= c <= high: res.append(c)
+        return res
 
 
-
-
-
-        
+# since sequential integer is limited, one possible solution is to generate all possible seqential integer --> O(1) time complexity
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        res = []
+        for i in range(1, 10):
+            c = i
+            for j in range(i+1, 10):
+                c = c * 10 + j
+                if low <= c <= high: res.append(c)
+        res.sort() # since length of res is limited, sorting takes O(1).
+        return res

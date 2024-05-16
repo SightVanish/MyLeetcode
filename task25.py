48db94995e6e76310526f96aded2227d09cca640
liwuchen <liwuchen@florens.com>
2024-05-16 16:06:43 +0800

    add

--- task25.py
+++ task25.py
@@ -1,20 +1,36 @@
+# follow task92 to handle this task
+from typing import List, Optional
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+def printNodes(head: ListNode):
+    while head: 
+        print(head.val)
+        head = head.next
 
 class Solution:
-    def reverseList(self, head, tail):
-        if head is None:
-            return head, head
-        if head.next is None:
-            return head, head
-        new_head, new_tail = self.reverseList(head.next, tail)
-        head.next = None
-        new_tail.next = head
-        return new_head, head
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None: return None 
+        p, num = head, 0
+        while p:
+            num += 1
+            p = p.next
+        dummy = ListNode(0, head)
+        end, pre, curr, next = dummy, dummy, dummy.next, dummy.next.next
+        for _ in range(num // k):
+            for _ in range(k - 1):
+                curr.next, pre, curr, next = pre, curr, next, next.next
+            new_end, curr.next, end.next.next, end.next = end.next, pre, next, curr
+            if new_end.next: end, pre, curr, next = new_end, new_end, new_end.next, new_end.next.next
+        return dummy.next
 
+
+
+
+
+            
 p1 = ListNode(1)
 p2 = ListNode(2)
 p3 = ListNode(3)
@@ -24,9 +40,5 @@ p1.next = p2
 p2.next = p3
 p3.next = p4
 p4.next = p5
-
 s = Solution()
-head, tail = s.reverseList(p1, p5)
-while head is not None:
-    print(head.val)
-    head = head.next
+printNodes(s.reverseKGroup(p1, 2))
--- task92.py
+++ task92.py
@@ -3,26 +3,31 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+def printNodes(head: ListNode):
+    while head: 
+        print(head.val)
+        head = head.next
 from typing import Optional
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        # l is the first node left to Node[left]
-        if left == 1: l = ListNode(0, head)
-        else:
-            l = head
-            for _ in range(1, left - 1): l = l.next
-        pre, end, curr, next = l.next, l.next, None, None
-        for _ in range(right - left):
-            curr, next = pre.next, pre.next.next
-            curr.next = pre
-        l.next = curr
-        end.next = next
-        while head: 
-            print(head.val)
-            head = head.next
-        return head
-
+        if head is None: return None 
+        dummy = ListNode(0, head)
+        pre = dummy # the first node left the Node[left]
+        for _ in range(left - 1): pre = pre.next
+        end, curr, next = pre, pre.next, pre.next.next
+        for _ in range(right - left): curr.next, pre, curr, next = pre, curr, next, next.next
+        curr.next, end.next.next, end.next = pre, next, curr
+        return dummy.next
             
-p1 = ListNode(5)
+p1 = ListNode(1)
+p2 = ListNode(2)
+p3 = ListNode(3)
+p4 = ListNode(4)
+p5 = ListNode(5)
+p1.next = p2
+p2.next = p3
+p3.next = p4
+p4.next = p5
+
 s = Solution()
-print(s.reverseBetween(p1, 1, 1))
+printNodes(s.reverseBetween(p1, 2, 4))

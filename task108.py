74e81567108dc7b1618a9cd5c2fd72b44c1c42b6
liwuchen <liwuchen@florens.com>
2024-06-17 15:16:17 +0800

    update

--- task108.py
+++ task108.py
@@ -7,4 +7,9 @@ class TreeNode:
 from typing import Optional, List
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        
+        if len(nums) == 0: return
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
